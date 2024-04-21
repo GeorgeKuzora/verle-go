39aa2acd71f6c781dcf446a73616a4783f06ff2c
GeorgiyKuzora <rafale87@gmail.com>
2024-04-21 20:05:25 +0300

    feat: add types in weeek package

---
+++ pkg/weeek/types.go
@@ -0,0 +1,16 @@
+package weeek
+
+import "time"
+
+// Task represents a weeek task card.
+type Task struct {
+	Id    int       `json:"id"`
+	Title string    `json: "title"`
+	Desc  string    `json:"description"`
+	Date  time.Time `json:"date"`
+}
+
+// DateTasks represents all tasks for a given date.
+type DateTasks struct {
+	Tasks []Task `json:"tasks"`
+}
