f0bf5324e9bf221c109f7e059f76d943c8a94b9f
GeorgiyKuzora <rafale87@gmail.com>
2024-05-10 16:21:45 +0300

    feat: Comment date field in Task type
    Until find solution for unmarshaling date

--- pkg/weeek/types.go
+++ pkg/weeek/types.go
@@ -1,13 +1,13 @@
 package weeek
 
-import "time"
+// import "time"
 
 // Task represents a weeek task card.
 type Task struct {
 	Id    int       `json:"id"`
 	Title string    `json: "title"`
 	Desc  string    `json:"description"`
-	Date  time.Time `json:"date"`
+	// Date  time.Time `json:"date"`
 }
 
 // DateTasks represents all tasks for a given date.
