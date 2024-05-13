d7bc2fd7ea087b7976843d86a305b3df637fe08e
GeorgiyKuzora <rafale87@gmail.com>
2024-05-13 21:49:00 +0300

    refactor: move DateTask to the begining of the file

--- pkg/sheets/types.go
+++ pkg/sheets/types.go
@@ -2,17 +2,17 @@ package sheets
 
 import "time"
 
+// DateTasks represents all tasks for a given date.
+type DateTasks struct {
+	Tasks []Task `json:"tasks"`
+}
+
 // Task represents a weeek task card.
 type Task struct {
 	Id    int    `json:"id"`
 	Title string `json:"title"`
 	Desc  string `json:"description"`
 	Date  Date   `json:"date"`
-}
-
-// DateTasks represents all tasks for a given date.
-type DateTasks struct {
-	Tasks []Task `json:"tasks"`
 }
 
 type Date time.Time
