8280138393dee180623b233a08e179d778cbe946
GeorgiyKuzora <rafale87@gmail.com>
2024-05-15 20:53:03 +0300

    fix: add sheets client init into main

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"verle_go/pkg/handlers"
 	"verle_go/pkg/server"
+	"verle_go/pkg/sheets"
 )
 
 func main() {
+	sheets.InitClient()
 	handlers.RegisterHandlers()
 	server.StartServer()
 }
