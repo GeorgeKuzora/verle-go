fe43592981d0e90a4830f5d961c368163e6018d1
GeorgiyKuzora <rafale87@gmail.com>
2024-05-20 21:09:14 +0300

    refactor: Remove unused handlers

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -14,35 +14,7 @@ func InitClients() {
 }
 
 func RegisterHandlers() {
-	http.HandleFunc("/read", readData)
-	http.HandleFunc("/create", createData)
-	http.HandleFunc("/update", updateData)
-	http.HandleFunc("/delete", deleteData)
-	http.HandleFunc("/getweeek", getDayTasks)
 	http.HandleFunc("/post", postDayTasks)
-}
-
-func readData(w http.ResponseWriter, r *http.Request) {
-	sheets.ReadData(w, r)
-}
-
-func createData(w http.ResponseWriter, r *http.Request) {
-	sheets.CreateData(w, r)
-}
-
-func updateData(w http.ResponseWriter, r *http.Request) {
-	sheets.UpdateData(w, r)
-}
-
-func deleteData(w http.ResponseWriter, r *http.Request) {
-	sheets.DeleteData(w, r)
-}
-
-func getDayTasks(w http.ResponseWriter, r *http.Request) {
-	tasks := weeek.GetWeekDayTasks("13.05.2024")
-	obj := weeek.UnmarshalDateTasks(tasks)
-	fmt.Printf("%+v\n", obj)
-	fmt.Println(time.Time(obj.Tasks[0].Date))
 }
 
 func postDayTasks(w http.ResponseWriter, r *http.Request) {
