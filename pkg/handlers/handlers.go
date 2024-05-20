73f0a1a0460a9a15558bfdea1ad64c315c904652
GeorgiyKuzora <rafale87@gmail.com>
2024-05-20 22:11:15 +0300

    feat: Add dates for 1 week to postDayTasks handler

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"net/http"
+	"time"
 	"verle_go/pkg/config"
 	"verle_go/pkg/converter"
 	"verle_go/pkg/sheets"
@@ -17,12 +18,17 @@ func RegisterHandlers() {
 }
 
 func postDayTasks(w http.ResponseWriter, r *http.Request) {
-	for _, wp := range config.Workplaces {
-		t := weeek.GetWeekDayTasks("16.05.2024", wp)
-		dayTasks := weeek.UnmarshalDateTasks(t)
-		// convert to sheets tasks
-		sheetsTasks := converter.ConvertWeeekSheets(dayTasks)
-		// write converted sheets tasks to sheets
-		sheets.WriteTasksToSheets(w, sheetsTasks, wp)
+	currentYear, currentMonth, currentDate := time.Now().Date()
+	current := time.Date(currentYear, currentMonth, currentDate, 1, 1, 1, 1, time.Local)
+	for i := 0; i <= 7; i++ {
+		for _, wp := range config.Workplaces {
+			t := weeek.GetWeekDayTasks(current.Format("02.01.2006"), wp)
+			dayTasks := weeek.UnmarshalDateTasks(t)
+			// convert to sheets tasks
+			sheetsTasks := converter.ConvertWeeekSheets(dayTasks)
+			// write converted sheets tasks to sheets
+			sheets.WriteTasksToSheets(w, sheetsTasks, wp)
+		}
+		current = current.AddDate(0, 0, 1)
 	}
 }
