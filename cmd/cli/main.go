cc40660f0d3890e3dceb35f2d4265c89c61c13b9
GeorgiyKuzora <rafale87@gmail.com>
2024-05-28 21:40:42 +0300

    feat: set log in file system

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"verle_go/pkg/handlers"
+	"verle_go/pkg/log"
 	"verle_go/pkg/server"
 )
 
 func main() {
+	log.LogInit()
 	handlers.InitClients()
 	handlers.RegisterHandlers()
 	server.StartServer()
--- pkg/log/log.go
+++ pkg/log/log.go
@@ -1,10 +1,53 @@
 package log
 
 import (
-	"os"
 	"log"
+	"os"
+)
+
+const (
+	logDirPath  = "logs"
+	logFileName = "app.log"
 )
 
 func LogInit() {
-	
+	file, err := getLogFileWriter(logDirPath, logFileName)
+	if err != nil {
+		log.Fatal("unexpected error during log file setup")
+	}
+	log.SetOutput(file)
+}
+
+func getLogFileWriter(path string, fileName string) (*os.File, error) {
+	filePath := path + "/" + fileName
+	logDirSetUp(path)
+	logFileSetUp(filePath)
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.Fatalf("unexpected error during creating log file Writer, on path: %s", filePath)
+	}
+	return file, err
+}
+
+func logDirSetUp(path string) {
+	_, err := os.Stat(path)
+	if os.IsNotExist(err) {
+		err := os.Mkdir(path, 0755)
+		if err != nil {
+			log.Fatalf("can't create log directory on path: %s ", path)
+		}
+	} else if err != nil {
+		log.Fatalf("unexpected error on path: %s", path)
+	}
+}
+
+func logFileSetUp(filePath string) {
+	_, err := os.Stat(filePath)
+	if os.IsNotExist(err) {
+		file, err := os.Create(filePath)
+		defer file.Close()
+		if err != nil {
+			log.Fatalf("can't create a log file on path: %s", filePath)
+		}
+	}
 }
