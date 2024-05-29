1e0891246bb47e7f8b6f6bbc7cd2c164dc7acc0f
GeorgiyKuzora <rafale87@gmail.com>
2024-05-29 14:36:43 +0300

    feat: Add tasks package with types module

---
+++ pkg/tasks/types.go
@@ -0,0 +1,44 @@
+package tasks
+
+import (
+	"time"
+)
+
+type Fetcher interface {
+	Fetch() (Tasks, error)
+}
+
+type Writer interface {
+	Write() error
+}
+
+// Project represents all open tasks for a production center.
+// Task is represented as a slice with tasks on future Dates
+type Project struct {
+	Dates        []Tasks `json:"dates"`
+	TasksFetcher Fetcher
+	TasksWriter  Writer
+}
+
+// Tasks represents all tasks for a given date.
+type Tasks struct {
+	Tasks []Task `json:"tasks"`
+}
+
+// Task represents a weeek task card. It includes:
+// Card Id, Title, Description, Date
+type Task struct {
+	Id    int    `json:"id"`
+	Title string `json:"title"`
+	Desc  string `json:"description"`
+	Date  Date   `json:"date"`
+}
+
+// Date represents Task date in proper format
+type Date time.Time
+
+func (d Date) String() string {
+	t := time.Time(d)
+	s := t.Format("02.01.2006")
+	return s
+}
